5fcf8ed43e1c3f5002aec482a6957cab0fadfc72
Steve Kondik <shade@chemlab.org>
2013-04-04 17:24:56 -0700

    hwui: This code should only be enabled with QCOM_BSP
     * Crashy crashy!
    
    Change-Id: I1a06ac54a8ce2dccbf4a67424b0cbb6e7dd21e8b

--- libs/hwui/OpenGLRenderer.cpp
+++ libs/hwui/OpenGLRenderer.cpp
@@ -253,7 +253,7 @@ void OpenGLRenderer::discardFramebuffer(float left, float top, float right, floa
 }
 
 status_t OpenGLRenderer::clear(float left, float top, float right, float bottom, bool opaque) {
-#ifdef QCOM_HARDWARE
+#ifdef QCOM_BSP
     mCaches.enableScissor();
     mCaches.setScissor(left, mSnapshot->height - bottom, right - left, bottom - top);
     glClear(GL_COLOR_BUFFER_BIT);
